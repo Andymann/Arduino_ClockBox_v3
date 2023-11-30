d1986f678dea37369642075b4097d95f08227bac
Andymann <info@andyland.info>
2023-11-30 08:08:21 +0100

    add sysex-mode

--- MidiClock_V3_Arduino.ino
+++ MidiClock_V3_Arduino.ino
@@ -2,6 +2,8 @@
 
 
 #include <SendOnlySoftwareSerial.h>
+#include <SoftwareSerial.h>
+
 #include <light_CD74HC4067.h>
 #include "TapTempo.h" // Note the quotation marks. Because it's in the same folder as the code itself. Get it, e.g. from https://github.com/Andymann/ArduinoTapTempo
 #include <uClock.h>
@@ -17,11 +19,8 @@
 // 0X3C+SA0 - 0x3C or 0x3D
 #define DISPLAY_I2C_ADDRESS 0x3C
 SSD1306AsciiWire oled;
-// Define proper RST_PIN if required.
-//#define DISPLAY_RST_PIN -1
 
-
-#define VERSION "3.01"
+#define VERSION "3.02"
 #define DEMUX_PIN A0
 
 //const int signal_pin = A0; // Pin Connected to Sig pin of CD74HC4067
@@ -56,11 +55,12 @@ Button2 btnHelper_PRESET2;
 Button2 btnHelper_PRESET3;
 
 //SoftwareSerial(rxPin, txPin, inverse_logic)
-//SoftwareSerial midi (A2, A3);
-SendOnlySoftwareSerial midi(A3);
+SoftwareSerial midi(A2, A3);
+//SendOnlySoftwareSerial midi(A3);
 
 uint8_t bpm_blink_timer = 1;
 float fBPM_Cache = 94.0;
+float fBPM_Sysex;
 //float fTempBPM = fBPM_Cache;
 bool bQuantizeRestartWaiting = false;
 uint8_t iMeasureCount = 0;
@@ -73,11 +73,19 @@ bool bIsPlaying = false;
 bool bUpdateStatusDisplay = false;
 bool bDisplayInverted = false;
 bool bNewBPM = false;
+bool bNudgeActive = false;
 
 String sActivePreset = "AAAAAAAAAAAAAA";
 bool bNewPresetSelected = false;
 
-byte muxValue[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // The value of the Tap button as read from the multiplexer
+#define CLOCKMODE_MASTER 1
+#define CLOCKMODE_MIXXX 2 
+uint8_t iClockMode = CLOCKMODE_MASTER;
+
+byte muxValue[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // The value of the Buttons as read from the multiplexer
+
+#define BUFFERSIZE 60
+byte sysexBuffer[BUFFERSIZE];
 
 //----Display
 //----SCL - 22
@@ -89,7 +97,7 @@ byte muxValue[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // The value of the Tap but
 void setup()
 {
   iNextPreset = NEXTPRESET_NONE;
-  //Serial.begin(9600);
+  Serial.begin(9600);
   //delay(251);
   pixels.begin();
   ledInit();
@@ -132,18 +140,21 @@ void setup()
   btnHelper_PRESET1.setLongClickTime( LONGCLICKTIMEMS );
   btnHelper_PRESET1.setClickHandler(preset1ClickHandler);
   btnHelper_PRESET1.setLongClickDetectedHandler(preset1LongClickDetected);
+  btnHelper_PRESET1.setChangedHandler(preset1ChangeHandler);  // Falls externer sync via sysex
 
   btnHelper_PRESET2.begin(VIRTUAL_PIN, INPUT, false);
   btnHelper_PRESET2.setButtonStateFunction(preset2ButtonStateHandler);
   btnHelper_PRESET2.setLongClickTime( LONGCLICKTIMEMS );
   btnHelper_PRESET2.setClickHandler(preset2ClickHandler);
   btnHelper_PRESET2.setLongClickDetectedHandler(preset2LongClickDetected);
+  btnHelper_PRESET2.setTapHandler(restartHandler);
 
   btnHelper_PRESET3.begin(VIRTUAL_PIN, INPUT, false);
   btnHelper_PRESET3.setButtonStateFunction(preset3ButtonStateHandler);
   btnHelper_PRESET3.setLongClickTime( LONGCLICKTIMEMS );
   btnHelper_PRESET3.setClickHandler(preset3ClickHandler);
   btnHelper_PRESET3.setLongClickDetectedHandler(preset3LongClickDetected);
+  btnHelper_PRESET3.setChangedHandler(preset3ChangeHandler);  // Falls externer sync via sysex
 
   Wire.begin();
   Wire.setClock(400000L);
@@ -167,43 +178,70 @@ void loop()
   btnHelper_PRESET3.loop();
 
   tapTempo.update(false);
-/*
-  if( tapTempo.getBPM() != fBPM_Cache){
-    fBPM_Cache = tapTempo.getBPM();
-    uClock.setTempo( fBPM_Cache );
-    if(bIsPlaying){
-      showBPM( fBPM_Cache );
-    }else{
-      showStop();
-    }
-  }
-*/
+
   // ---- Nur ganzzahlige Werte darstellen, Rundungsfehler ueberdecken
-  if( abs(tapTempo.getBPM() - fBPM_Cache) >= 1.0){
-    bNewBPM = true;
-    fBPM_Cache = uint8_t(tapTempo.getBPM());
-    uClock.setTempo( fBPM_Cache );
-    //tapTempo.setBPM( fBPM_Cache );
-    if(bIsPlaying){
-      showBPM( fBPM_Cache );
-    }else{
-      showStatus( 0, false );
+  if(iClockMode==CLOCKMODE_MASTER){
+    if( abs(tapTempo.getBPM() - fBPM_Cache) >= 1.0){
+      bNewBPM = true;
+      fBPM_Cache = uint8_t(tapTempo.getBPM());
+      uClock.setTempo( fBPM_Cache );
+      //tapTempo.setBPM( fBPM_Cache );
+      if(bIsPlaying){
+        showBPM( fBPM_Cache );
+      }else{
+        showStatus( 0, false );
+      }
+    }
+  }else if(iClockMode==CLOCKMODE_MIXXX){
+    //Serial.print("tapTempo.getBPM()"); Serial.println(tapTempo.getBPM());
+    //Serial.print("fBPM_Sysex"); Serial.println( fBPM_Sysex );
+    //Serial.print("clock.getTempo()"); Serial.println( uClock.getTempo() );
+    if( abs(uClock.getTempo() - fBPM_Sysex) >= .1){
+      if( bNudgeActive==false ){
+        fBPM_Cache = fBPM_Sysex;
+        setGlobalBPM(fBPM_Sysex);
+        if(bIsPlaying){
+          showBPM( fBPM_Sysex );
+        }else{
+          showStatus( 0, false );
+        }
+      }else{
+        if(bIsPlaying){
+          showBPM( fBPM_Cache );
+        }else{
+          showStatus( 0, false );
+        }
+      }
     }
   }
+  
 
   int iEncoder = queryEncoder();
   if(iEncoder!=0){
-    if(muxValue[13]==0){
-      //setGlobalBPM( /*tapTempo.getBPM()*/ fBPM_Cache + (iEncoder*1.0) );
-      fBPM_Cache += iEncoder;
-    }else{
-      //setGlobalBPM( /*tapTempo.getBPM()*/ fBPM_Cache + (iEncoder*5.0) );
-      fBPM_Cache += iEncoder*5.0;
+    if(iClockMode==CLOCKMODE_MASTER){
+      if(muxValue[13]==0){
+        //setGlobalBPM( /*tapTempo.getBPM()*/ fBPM_Cache + (iEncoder*1.0) );
+        fBPM_Cache += iEncoder;
+      }else{
+        //setGlobalBPM( /*tapTempo.getBPM()*/ fBPM_Cache + (iEncoder*5.0) );
+        fBPM_Cache += iEncoder*5.0;
+      }
+      bNewBPM = true;
+      setGlobalBPM( fBPM_Cache);
+    }else if(iClockMode==CLOCKMODE_MIXXX){
+      if(muxValue[13]==0){
+        //setGlobalBPM( /*tapTempo.getBPM()*/ fBPM_Cache + (iEncoder*1.0) );
+        fBPM_Sysex += iEncoder*0.01;
+        fBPM_Cache = fBPM_Sysex;
+      }else{
+        //setGlobalBPM( /*tapTempo.getBPM()*/ fBPM_Cache + (iEncoder*5.0) );
+        fBPM_Sysex += iEncoder*1.0;
+        fBPM_Cache = fBPM_Sysex;
+      }
+      bNewBPM = true;
+      setGlobalBPM( fBPM_Cache);
     }
-    bNewBPM = true;
-    tapTempo.setBPM( fBPM_Cache );
-    uClock.setTempo( fBPM_Cache );
-    showBPM( fBPM_Cache );
+    
   }
 
   if(iNextPreset != NEXTPRESET_NONE){
@@ -217,23 +255,91 @@ void loop()
     oled.setFont(ZevvPeep8x16);
     oled.set1X();
     oled.setRow(6);
-    oled.setCol(105);
-    oled.print("BPM");
+    if( iClockMode == CLOCKMODE_MASTER){
+      oled.setCol(105);
+      oled.print("BPM");
+    }else if( iClockMode == CLOCKMODE_MIXXX ){
+      oled.setCol(88);
+      oled.print("SYSEX");
+    }
     bUpdateStatusDisplay=false;
   }
 
     if(bNewPresetSelected == true){
-    oled.setInvertMode( false );
-    oled.setFont(ZevvPeep8x16);
-    oled.set1X();
-    oled.setRow(6);
-    oled.setCol(5);
-    oled.print(sActivePreset);
-    bNewPresetSelected = false;
+      oled.setInvertMode( false );
+      oled.setFont(ZevvPeep8x16);
+      oled.set1X();
+      oled.setRow(6);
+      oled.setCol(5);
+      oled.print(sActivePreset);
+      bNewPresetSelected = false;
+    }
+
+  midiEventPacket_t rx = MidiUSB.read();
+  if (rx.header != 0) {
+    processMidiIn( rx );
   }
-
-
 }//
+
+void processMidiIn(midiEventPacket_t pRX){
+  switch (pRX.header & 0x0F) {
+    case 0x00:  // Misc. Reserved for future extensions.
+      break;
+    case 0x01:  // Cable events. Reserved for future expansion.
+      break;
+    case 0x02:  // Two-byte System Common messages
+      break;
+    case 0x0C:  // Program Change
+      break;
+    case 0x0D:  // Channel Pressure
+      // do something with 2 byte messages
+      break;
+    case 0x03:  // Three-byte System Common messages
+      break;
+    case 0x08:  // Note-off
+      break;
+    case 0x09:  // Note-on
+      break;
+    case 0x0A:  // Poly-KeyPress
+      break;
+    case 0x0B:  // Control Change
+      break;
+    case 0x0E:  // PitchBend Change
+      // do something with 3 byte messages
+      break;
+    case 0x04:  // SysEx starts or continues
+      // append sysex buffer with 3 bytes
+      iClockMode = CLOCKMODE_MIXXX;
+      fillSysexBuffer( pRX, 3 );
+      break;
+    case 0x05:  // Single-byte System Common Message or SysEx ends with the following single byte
+      // append sysex buffer with 1 byte
+      // process completed sysex buffer
+      // init sysex buffer empty
+      fillSysexBuffer( pRX, 1 );
+      Serial.println("header 0x05");
+      break;
+    case 0x06:  // SysEx ends with the following two bytes
+      // append sysex buffer with 2 bytes
+      // process completed sysex buffer
+      // init sysex buffer empty
+      fillSysexBuffer( pRX, 2 );
+      processSysexBuffer();
+      //Serial.println("header 0x06");
+      break;
+    case 0x07:  // SysEx ends with the following three bytes
+      // append sysex buffer with 3 bytes
+      // process completed sysex buffer
+      // init sysex buffer empty
+      fillSysexBuffer( pRX, 3 );
+      Serial.println("header 0x07");
+      break;
+    case 0x0F:  // Single Byte, TuneRequest, Clock, Start, Continue, Stop, etc.
+      // process since byte messages
+      break;
+    
+  }
+}
 
 void selectPreset(uint8_t pPresetID){
   if(pPresetID == NEXTPRESET_1){
@@ -360,7 +466,18 @@ byte tapButtonStateHandler() {
 }
 
 void startHandler(Button2& btn) {
-    startPlaying();
+  startPlaying();
+}
+
+void restartHandler(Button2& btn){
+  if(iClockMode==CLOCKMODE_MIXXX){
+    //stopPlaying();
+    //startPlaying();
+    bNewBPM = true;
+    showBPM( fBPM_Cache );
+    uClock.start();
+  }
+  
 }
 
 void startPlaying(){
@@ -400,15 +517,31 @@ byte preset1ButtonStateHandler() {
 }
 
 void preset1ClickHandler(Button2& btn) {
-    //Serial.println("Preset1 clicked");
+  if(iClockMode == CLOCKMODE_MASTER){
     iNextPreset = NEXTPRESET_1;
+  }
 }
 
 void preset1LongClickDetected(Button2& btn) {
-    //Serial.print("Preset 1 long click detected");
-    fBPM_Preset1 = tapTempo.getBPM();
-    EEPROM.update(10, byte(fBPM_Preset1));
-    ledRed();
+    if(iClockMode == CLOCKMODE_MASTER){
+      fBPM_Preset1 = tapTempo.getBPM();
+      EEPROM.update(10, byte(fBPM_Preset1));
+      ledRed();
+    }
+}
+
+void preset1ChangeHandler( Button2& btn ){
+  if(iClockMode == CLOCKMODE_MIXXX){
+    //Serial.println("Preset1 Button changed " + String(btn.isPressed() ) );
+    nudgeMinus( btn.isPressed() );
+  }
+}
+
+void preset3ChangeHandler( Button2& btn ){
+  if(iClockMode == CLOCKMODE_MIXXX){
+    //Serial.println("Preset2 Button changed " + String(btn.isPressed() ) );
+    nudgePlus( btn.isPressed() );
+  }
 }
 
 byte preset2ButtonStateHandler() {
@@ -416,15 +549,17 @@ byte preset2ButtonStateHandler() {
 }
 
 void preset2ClickHandler(Button2& btn) {
-  //Serial.println("Preset2 clicked");
-  iNextPreset = NEXTPRESET_2;
+  if(iClockMode == CLOCKMODE_MASTER){
+    iNextPreset = NEXTPRESET_2;
+  }
 }
 
 void preset2LongClickDetected(Button2& btn) {
-    //Serial.println("Preset 2 long click detected");
+  if(iClockMode == CLOCKMODE_MASTER){
     fBPM_Preset2 = tapTempo.getBPM();
     EEPROM.update(20, byte(fBPM_Preset2));
     ledRed();
+  }
 }
 
 byte preset3ButtonStateHandler() {
@@ -432,15 +567,17 @@ byte preset3ButtonStateHandler() {
 }
 
 void preset3ClickHandler(Button2& btn) {
-  //Serial.println("Preset3 clicked");
-  iNextPreset = NEXTPRESET_3;
+  if(iClockMode == CLOCKMODE_MASTER){
+    iNextPreset = NEXTPRESET_3;
+  }
 }
 
 void preset3LongClickDetected(Button2& btn) {
-    //Serial.println("Preset 3 long click detected");
+  if(iClockMode == CLOCKMODE_MASTER){
     fBPM_Preset3 = tapTempo.getBPM();
     EEPROM.update(30, byte(fBPM_Preset3));
     ledRed();
+  }
 }
 
 void ledIndicateStart(){
@@ -452,7 +589,10 @@ void ledIndicateStart(){
 }
 
 void setGlobalBPM(float f){
-  tapTempo.setBPM( f );
+  bNewBPM = true;
+  if(iClockMode==CLOCKMODE_MASTER){
+    tapTempo.setBPM( f );
+  }
   uClock.setTempo( f );
   //showBPM( f );
 }
@@ -500,17 +640,17 @@ void ledInit(){
 void getPresetsFromEeprom(){
   uint8_t val;
   val = EEPROM.read(10);
-  if(val!=255){ // a.k.a. hier wurde schonmla etwas gespeichert
+  if(val!=255){ // a.k.a. hier wurde schonmal etwas gespeichert
     fBPM_Preset1 = val;
   }
 
   val = EEPROM.read(20);
-  if(val!=255){ // a.k.a. hier wurde schonmla etwas gespeichert
+  if(val!=255){
     fBPM_Preset2 = val;
   }
 
   val = EEPROM.read(30);
-  if(val!=255){ // a.k.a. hier wurde schonmla etwas gespeichert
+  if(val!=255){
     fBPM_Preset3 = val;
   }
 }
@@ -570,7 +710,11 @@ void testDisplay(){
 
 void showBPM(float p){
   oled.setInvertMode( false );
-  oled.setFont(Verdana_digits_24);
+  if(iClockMode==CLOCKMODE_MASTER){
+    oled.setFont(Verdana_digits_24);
+  }else if(iClockMode==CLOCKMODE_MIXXX){
+
+  }
   if( bNewBPM ){
     oled.clear();
     bNewBPM = false;
@@ -583,42 +727,19 @@ void showBPM(float p){
   
   oled.set2X();
   oled.setRow(0);
-  if(fBPM_Cache<100){
+  if(p<100){
     oled.setCol(30);
   }else{
     oled.setCol(15);
   }
   oled.print( String(p) );
 
-  
-
-  
 }
 
 
 void showStatus(int iMeasure, bool pInverted){
   bDisplayInverted = pInverted;
   bUpdateStatusDisplay = true;
-  /*
-  oled.setInvertMode( pInverted );
-  //oled.set2X();
-  oled.set1X();
-  
-  //oled.setFont(ZevvPeep8x16);
-  //oled.setCol( 35 );
-  //oled.setRow( 6 );
-  if(pInverted){
-    //oled.print( "Playing" );
-    oled.home();
-    oled.print(String(iMeasure));
-    
-  }else{
-    //oled.print( "Stopped" );
-    oled.home();
-    oled.print(' ');
-  }
-  oled.setInvertMode( false );
-  */
 }
 
 
@@ -632,4 +753,99 @@ void showInfo(int pWaitMS){
   oled.println();
   oled.println(" Andyland.info");
   delay(pWaitMS);
+}
+
+
+void fillSysexBuffer(midiEventPacket_t pRX, uint8_t pSize){
+  //Serial.println("received data " + String(pRX.byte1, 16));
+  if(pRX.byte1 == 0xF0){
+    //Serial.println("Clear buffer");
+    clearSysexBuffer();
+  }
+
+  for(int i=0; i<BUFFERSIZE; i++){
+    if(sysexBuffer[i]==0xFF){
+      if( pSize==1 ){
+        sysexBuffer[i]=pRX.byte1;
+      }else if ( pSize==2 ) {
+        sysexBuffer[i]=pRX.byte1;
+        sysexBuffer[i+1]=pRX.byte2;
+      }else if( pSize==3 ){
+        sysexBuffer[i]=pRX.byte1;
+        sysexBuffer[i+1]=pRX.byte2;
+        sysexBuffer[i+2]=pRX.byte3;
+      }
+      break; //exit for
+    }
+    if(sysexBuffer[i] == 0xF7){
+      processSysexBuffer();
+    }
+  }
+}
+
+void processSysexBuffer(){
+  uint8_t x=0;
+  for(int i=0; i<BUFFERSIZE; i++){
+    if(sysexBuffer[i] != 0xFF){
+      x++;
+    }
+  }
+  if(x > 10){
+    //Serial.println("Sysex Buffer processing: " + String(x));
+    //"0x7F 0x01 0x1"; BPM 
+    for(int i=0; i<BUFFERSIZE; i++){
+      if((sysexBuffer[i] == 0x7F) && (sysexBuffer[i+1] == 0x01) && (sysexBuffer[i+2] == 0x11)){
+        //BPM Deck 1
+        
+        String s = String(sysexBuffer[i+3], 16) + String(sysexBuffer[i+4], 16) + String(sysexBuffer[i+5], 16) + String(sysexBuffer[i+6], 16) + String(sysexBuffer[i+7], 16);
+        //float fBPM = s.toInt();
+        float temp = s.toInt();
+        temp /= 100.0;
+        if(temp > 10.0 ){
+          fBPM_Sysex = temp;
+          fBPM_Cache = temp;
+          //bNewBPM = true;
+          setGlobalBPM( fBPM_Sysex );
+          uClock.start();
+          //showBPM(fBPM_Sysex);
+        }
+        Serial.print("Found BPM: ");
+        Serial.println(String(fBPM_Sysex, 10));
+
+        break;
+      }
+    }
+  }
+}
+
+void clearSysexBuffer(){
+  for(int i=0; i<BUFFERSIZE; i++){
+    sysexBuffer[i] = 0xFF;
+  }
+}
+
+void nudgePlus(bool pOnOff){
+  if(pOnOff){
+    //fBPM_NudgeCache = tapTempo.getBPM();//uClock.getTempo();
+    bNudgeActive=true;
+    fBPM_Cache *= 1.05;
+    setGlobalBPM( fBPM_Cache);
+  }else{
+    bNudgeActive = false;
+    fBPM_Cache = fBPM_Sysex;
+    setGlobalBPM( fBPM_Cache );
+  }
+}
+
+void nudgeMinus(bool pOnOff){
+  if(pOnOff){
+    //fBPM_NudgeCache = tapTempo.getBPM(); //uClock.getTempo();
+    bNudgeActive = true;
+    fBPM_Cache *= 0.95;
+    setGlobalBPM( fBPM_Cache );
+  }else{
+    fBPM_Cache = fBPM_Sysex;
+    setGlobalBPM( fBPM_Cache );
+    bNudgeActive = false;
+  }
 }
