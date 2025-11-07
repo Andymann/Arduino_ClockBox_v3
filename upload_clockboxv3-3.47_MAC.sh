0ab4abd8bc58f4f9c5103620c44928360d907ead
Andy Fischer <info@andyland.info>
2025-11-07 21:07:25 +0100

    add upload script

---
+++ upload_clockboxv3-3.47_MAC.sh
@@ -0,0 +1,8 @@
+curl -OL https://raw.githubusercontent.com/Andymann/Arduino_ClockBox_v3/refs/heads/main/clockboxV3-3.47.hex
+curl -OL https://github.com/avrdudes/avrdude/releases/download/v8.1/avrdude_v8.1_macOS_64bit.tar.gz
+tar -xvzf $(ls avrdude*)
+stty -f $(ls /dev/tty.usbmodem*) 1200
+sleep 1
+PORT=$(ls /dev/tty.usbmodem*)
+echo $PORT
+./avrdude_macOS_64bit/bin/avrdude -p atmega32u4 -c avr109 -P $PORT -b 57600 -U flash:w:clockboxV3-3.47.hex:i
